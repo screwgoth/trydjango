6edd2eed3d4eefd0686bcacca66cab76ea771946
screwgoth <raseelbhagat@gmail.com>
2022-02-15 20:06:23 +0530

    Finished with basics of Forms - Video 27 of the Tutorial Serier

--- articles/forms.py
+++ articles/forms.py
@@ -5,18 +5,24 @@ class ArticleForm(forms.Form):
 	content = forms.CharField()
 
 	# Methods to clean data, clean_fieldname
-	def clean_title(self):
-		# self.cleaned_data is existing property
-		cleaned_data = self.cleaned_data
-		print('cleaned_data: ', cleaned_data)
-		title = cleaned_data.get('title')
-		print('title: ', title)
-		if title.lower().strip() == 'ugly title':
-			raise forms.ValidationError('Title cannot be "ugly title"')
-		return title
+	# def clean_title(self):
+	# 	# self.cleaned_data is existing property
+	# 	cleaned_data = self.cleaned_data #dictionary
+	# 	print('cleaned_data: ', cleaned_data)
+	# 	title = cleaned_data.get('title')
+	# 	print('title: ', title)
+	# 	if title.lower().strip() == 'ugly title':
+	# 		raise forms.ValidationError('Title cannot be "ugly title"')
+	# 	return title
 
 	# Clean all the fields
 	def clean(self):
 		cleaned_data = self.cleaned_data
 		print('All cleaned_data: ', cleaned_data)
+		title = cleaned_data.get('title')
+		print('title: ', title)
+		if title.lower().strip() == 'ugly title':
+			raise forms.ValidationError('Title cannot be "ugly title"')
 		return cleaned_data
+
+		
--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,3 @@
-from stat import FILE_ATTRIBUTE_COMPRESSED
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Articles
@@ -7,30 +6,48 @@ from .forms import ArticleForm
 # login_requried decorator to protect creating Articles by any visitor.
 @login_required
 def article_create_view(request):
-	#print(request.POST)
-	form = ArticleForm()
-	#print(dir(form))
+	form = ArticleForm(request.POST or None)
 	context = {
-		"form": form,
+		'form': form,
 	}
-	if request.method == 'POST':
-		form = ArticleForm(request.POST)
-		# Set the contenxt, so we can show the validation error if the Form is not clean
-		context['form'] = form
-		# is_valid() checks against all the "clean()" methods of the ArticleForm class
-		if form.is_valid():
-			title = form.cleaned_data.get('title')
-			content = form.cleaned_data.get('content')
-			# title = request.POST.get('title')
-			# content = request.POST.get('content')
-			#print(title, content)
-			article_object = Articles.objects.create(title=title, content=content)
-			# This is also one way to populate context
-			context['object'] = article_object
-			# Context can have any fields.
-			context['created'] = True
+	# is_valid() checks against all the "clean()" methods of the ArticleForm class
+	if form.is_valid():
+		title = form.cleaned_data.get('title')
+		content = form.cleaned_data.get('content')
+		article_object = Articles.objects.create(title=title, content=content)
+		# This is also one way to populate context
+		context['object'] = article_object
+		# Context can have any fields.
+		context['created'] = True
 		
 	return render(request, "articles/create.html", context=context)
+
+# Reference class for creating a view without full use of Forms	
+# def article_create_view(request):
+# 	#print(request.POST)
+# 	form = ArticleForm()
+# 	#print(dir(form))
+# 	context = {
+# 		"form": form,
+# 	}
+# 	if request.method == 'POST':
+# 		form = ArticleForm(request.POST)
+# 		# Set the contenxt, so we can show the validation error if the Form is not clean
+# 		context['form'] = form
+# 		# is_valid() checks against all the "clean()" methods of the ArticleForm class
+# 		if form.is_valid():
+# 			title = form.cleaned_data.get('title')
+# 			content = form.cleaned_data.get('content')
+# 			# title = request.POST.get('title')
+# 			# content = request.POST.get('content')
+# 			#print(title, content)
+# 			article_object = Articles.objects.create(title=title, content=content)
+# 			# This is also one way to populate context
+# 			context['object'] = article_object
+# 			# Context can have any fields.
+# 			context['created'] = True
+		
+# 	return render(request, "articles/create.html", context=context)
 
 def article_search_view(request):
 	# print(request)
